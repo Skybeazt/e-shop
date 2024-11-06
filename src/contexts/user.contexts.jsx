7bef37a7080981894798329f37a165a7f5c4de50
Paul Ariku <arikupaul9@gmail.com>
2024-11-06 13:23:10 +0000

    Replaced the useState hook in the user context with useReducer hook

--- src/contexts/user.contexts.jsx
+++ src/contexts/user.contexts.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import {
   onAuthStateChangeListener,
   createUserDocumentFromAuth,
@@ -9,8 +9,38 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = function (state, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+
+    default: {
+      throw new Error(`Unhandled type ${type}`);
+    }
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 export const UserContextProvider = function ({ children }) {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = function (user) {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(function () {

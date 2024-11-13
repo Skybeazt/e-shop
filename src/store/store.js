1611e04b6851d3934a0393e05e01ab125f3f090b
Paul Ariku <arikupaul9@gmail.com>
2024-11-13 12:59:57 +0000

    Added the redux dev tool chrome extension

--- src/store/store.js
+++ src/store/store.js
@@ -4,18 +4,6 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-const loggerMiddleWare = (store) => (next) => (action) => {
-  if (!action.type) return next(action);
-
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
-  console.log("currentState: ", store.getState());
-
-  next(action);
-
-  console.log("next state: ", store.getState());
-};
-
 const persistConfig = {
   key: "root",
   storage,
@@ -24,9 +12,17 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleWare];
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
 
-const composeEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composeEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composeEnhancers);
 

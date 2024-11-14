c5a693f7d4e1cfc930bf56ce9013329ce48c8f2f
Paul Ariku <arikupaul9@gmail.com>
2024-11-14 10:53:28 +0000

    Added and untilise the redux-thunk npm package
    Redux-thunk been implemented for the categories redux and also added a loading spinner using the loading property of the categories redux

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,19 +1,28 @@
 import { Fragment } from "react";
 import { useSelector } from "react-redux";
-import { selectCategoriesMap } from "./../../store/categories/category.selector.js";
+import {
+  selectCategoriesMap,
+  selectCategoriesIsLoading,
+} from "./../../store/categories/category.selector.js";
 
 import CategoryPreview from "../../components/category-preview/category-preview.component";
+import Spinner from "../../components/spinner/spinner.component.jsx";
 
 const CategoriesPreview = function () {
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isCategoriesLoadinig = useSelector(selectCategoriesIsLoading);
   return (
     <Fragment>
-      {Object.keys(categoriesMap).map(function (title) {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} products={products} title={title} />
-        );
-      })}
+      {isCategoriesLoadinig ? (
+        <Spinner />
+      ) : (
+        Object.keys(categoriesMap).map(function (title) {
+          const products = categoriesMap[title];
+          return (
+            <CategoryPreview key={title} products={products} title={title} />
+          );
+        })
+      )}
     </Fragment>
   );
 };
--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,6 +1,31 @@
+import { getCollectionAndDocuments } from "../../utils/firebase/firebase.utils.js";
+
 import { CATEGORIES_ACTION_TYPES } from "./category.types.js";
 import { createAction } from "../../utils/reducer/reducer.utils";
 
-export const setCategories = function (categoriesArray) {
-  return createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
+export const fetchCategoriesStart = function () {
+  return createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+};
+
+export const fetchCategoriesFailed = function (error) {
+  return createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+};
+
+const fetchCategoriesSuccess = function (categoriesArray) {
+  return createAction(
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+    categoriesArray
+  );
+};
+
+export const fetchCategoriesAsync = function () {
+  return async function (dispatch) {
+    dispatch(fetchCategoriesStart());
+    try {
+      const categoriesArray = await getCollectionAndDocuments();
+      dispatch(fetchCategoriesSuccess(categoriesArray));
+    } catch (error) {
+      dispatch(fetchCategoriesFailed(error));
+    }
+  };
 };
